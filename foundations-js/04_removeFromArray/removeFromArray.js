35a26129ca4c360f9dcf9ce06870981ce3f6cb26
flapjackstan <elm.s.camargo@gmail.com>
2022-05-09 21:13:09 -0700

    exercise 4- more comments

--- foundations-js/04_removeFromArray/removeFromArray.js
+++ foundations-js/04_removeFromArray/removeFromArray.js
@@ -2,7 +2,10 @@ const removeFromArray = function(unfilteredArray, ...elementToFilter) {
 // ensure an array and an element is passed into the function
 if(Array.isArray(unfilteredArray)){
 	// create an array that does not include the element 
-	return unfilteredArray.filter((element) => !elementToFilter.includes( element ))
+	return unfilteredArray.filter((element) => {
+
+		// .includes returns a boolean. putting the ! turns trues into false and false into true
+		return !elementToFilter.includes( element )})
 }else{
 	return 'NOT AN ARRAY'
 }
