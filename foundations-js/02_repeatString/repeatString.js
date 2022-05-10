c21cb032de333f9c1378f8cbfa61350be8bc7529
flapjackstan <elm.s.camargo@gmail.com>
2022-05-09 20:30:24 -0700

    exercise 2

--- foundations-js/02_repeatString/repeatString.js
+++ foundations-js/02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(stringToRepeat, timesToRepeat) {
+	let combinedString = ''
 
+	if(timesToRepeat < 0){
+		return 'ERROR';
+	}
+
+	for(let timesRepeated = 0; timesRepeated < timesToRepeat; timesRepeated++){
+		combinedString = combinedString + stringToRepeat
+	}
+	return combinedString;
 };
 
 // Do not edit below this line
