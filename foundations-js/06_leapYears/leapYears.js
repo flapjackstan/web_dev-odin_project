d900a496d309dfde2887a8ed9efc13877e5b60c8
flapjackstan <elm.s.camargo@gmail.com>
2022-05-11 19:05:25 -0700

    solution

--- foundations-js/06_leapYears/leapYears.js
+++ foundations-js/06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
+const leapYears = function(year) {
+	// checks if year is divisible by 4
+	let isLeapYear = ((year % 400 === 0) || (year % 4 === 0 && year % 100 != 0));
 
+	// returns final bool
+	return isLeapYear;
 };
 
 // Do not edit below this line
